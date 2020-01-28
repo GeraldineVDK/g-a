879b8803d5802d72b1e221ecccdbea6479a7a52d
Geraldine Vanderkelen <g.vanderkelen@gmail.com>
2020-01-28 11:29:05 +0100

    adding review model, controller, view and form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,4 +10,5 @@ class PagesController < ApplicationController
 
   def home
   end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :writer, presence: true
-  validates :rating, presence: true, inclusion: { in: ["1", "2", "3", "4", "5"]}
+  validates :rating, presence: true, inclusion: { in: (1..5)}
 end
